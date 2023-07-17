214e07d9f1ef5f0b0d2ae7b3e06e8c70aec26348
anuragjatofficial <anurag.1101140@gmail.com>
2023-07-18 02:37:08 +0530

    updated bytecoce

--- swiggy2/src/main/java/com/swiggy/exceptions/SwiggyException.java
+++ swiggy2/src/main/java/com/swiggy/exceptions/SwiggyException.java
@@ -1,6 +1,11 @@
 package com.swiggy.exceptions;
 
 public class SwiggyException extends RuntimeException {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public SwiggyException(String str) {
 		super(str);
 	}
